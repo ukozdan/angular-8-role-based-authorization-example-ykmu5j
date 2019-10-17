18692f1a79d77bd552b6b527117b25617976b928
ukozdan <danroysanders@gmail.com>
2019-10-17 22:11:14 +0100

    2 files modified, 1 added

--- src/app/incident/incident.component.ts
+++ src/app/incident/incident.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from "../_helpers/must-match.validator";
 
 @Component({
   selector: "app-incident",
@@ -6,7 +10,49 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./incident.component.css"]
 })
 export class IncidentComponent implements OnInit {
-  constructor() {}
+  registerForm: FormGroup;
+  submitted = false;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        title: ["", Validators.required],
+        firstName: ["", Validators.required],
+        lastName: ["", Validators.required],
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validator: MustMatch("password", "confirmPassword")
+      }
+    );
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert(
+      "SUCCESS!! :-)\n\n" + JSON.stringify(this.registerForm.value, null, 4)
+    );
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
