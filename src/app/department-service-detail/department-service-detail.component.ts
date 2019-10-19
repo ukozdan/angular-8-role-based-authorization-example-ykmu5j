1d889358e40ca09aff1a8ab7ce6316552dc9012b
ukozdan <danroysanders@gmail.com>
2019-10-19 22:31:19 +0100

    3 files modified

--- src/app/department-service-detail/department-service-detail.component.ts
+++ src/app/department-service-detail/department-service-detail.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { DepartmentService } from "../_models/department-service";
+import { DepartmentServiceService } from "../_services/department-service.service";
 
 @Component({
-  selector: 'app-department-service-detail',
-  templateUrl: './department-service-detail.component.html',
-  styleUrls: ['./department-service-detail.component.css']
+  selector: "app-department-service-detail",
+  templateUrl: "./department-service-detail.component.html",
+  styleUrls: ["./department-service-detail.component.css"]
 })
 export class DepartmentServiceDetailComponent implements OnInit {
+  @Input() departmentService: DepartmentService;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private departmentServiceService: DepartmentServiceService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getDepartmentService();
   }
 
+  getDepartmentService(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.departmentServiceService
+      .getDepartmentService(id)
+      .subscribe(
+        departmentService => (this.departmentService = departmentService)
+      );
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.departmentServiceService
+      .updateDepartmentService(this.departmentService)
+      .subscribe(() => this.goBack());
+  }
 }
--- src/app/department-service-search/department-service-search.component.ts
+++ src/app/department-service-search/department-service-search.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Observable, Subject } from "rxjs";
+
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
+
+import { DepartmentService } from "../_models/department-service";
+import { DepartmentServiceService } from "../_services/department-service.service";
 
 @Component({
-  selector: 'app-department-service-search',
-  templateUrl: './department-service-search.component.html',
-  styleUrls: ['./department-service-search.component.css']
+  selector: "app-department-service-search",
+  templateUrl: "./department-service-search.component.html",
+  styleUrls: ["./department-service-search.component.css"]
 })
 export class DepartmentServiceSearchComponent implements OnInit {
+  departmentServices$: Observable<DepartmentService[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private departmentServiceService: DepartmentServiceService) {}
 
-  ngOnInit() {
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.departmentServices$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) =>
+        this.departmentServiceService.searchDepartmentService(term)
+      )
+    );
+  }
 }
